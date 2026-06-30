42930f8dd4634e88eacc673afef468aee1c8d6ed
sloviso03 <sloviso03@gmail.com>
2026-06-30 15:34:10 -0300

    Dependencies & packages

--- folders.sh
+++ folders.sh
@@ -2,35 +2,31 @@
 
 export SYSTEM_TIMEZONE=$(timedatectl show | grep Timezone | cut -d'=' -f2)
 
-
 mkdir -p ~/.config/sway
 cp sway/* ~/.config/sway
-
 
 mkdir -p ~/.config/alacritty
 cp alacritty/* ~/.config/alacritty
 
-
 mkdir -p ~/.config/waybar
-envsubst < waybar/config.jsonc > ~/.config/waybar/config.jsonc
+cp waybar/config.jsonc ~/.config/waybar/config.jsonc
+sed -i "s|\${SYSTEM_TIMEZONE}|$SYSTEM_TIMEZONE|g" ~/.config/waybar/config.jsonc
 cp waybar/style.css ~/.config/waybar/style.css
-
 
 mkdir -p ~/.config/fastfetch
 cp fastfetch/* ~/.config/fastfetch/config.jsonc
-
 
 xdg-mime default ranger.desktop inode/directory
 mkdir -p ~/.local/share/applications
 cp ranger/* ~/.local/share/applications/
 
-
 cp -f bash/.bashrc "$HOME/.bashrc"
 source "$HOME/.bashrc"
 
-
-echo "Setting micro as default editor in Debian..."
 sudo update-alternatives --set editor /usr/bin/micro
 
 mkdir -p ~/.config/micro
 cp -r micro/* ~/.config/micro/
+
+systemctl --user daemon-reload
+systemctl --user enable --now pipewire pipewire-pulse wireplumber
