723780badd5506cd4276c77f96239b954692544d
sloviso03 <sloviso03@gmail.com>
2026-06-30 14:42:52 -0300

    packages.sh

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt install sway fonts-jetbrains-mono waybar alacritty grim fzf slurp swappy wl-clipboard ranger udiskie udisks2 wlogout greetd swaylock
+sudo apt install sway fonts-jetbrains-mono waybar alacritty grim fzf slurp swappy wl-clipboard ranger udiskie udisks2
