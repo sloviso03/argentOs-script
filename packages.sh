ba7442671ccbd901b3d0fd1699632cbda3a06d52
sloviso03 <sloviso03@gmail.com>
2026-07-24 11:09:17 -0300

    testing

--- packages.sh
+++ packages.sh
@@ -5,14 +5,14 @@ sudo apt update
 sudo apt install -y \
   git unzip sway autotiling fonts-jetbrains-mono fzf micro fastfetch \
   network-manager network-manager-gnome bluez bluez-tools blueman \
-  power-profiles-daemon upower audacious \
+  power-profiles-daemon upower vlc \
   pipewire-audio wireplumber pipewire-pulse pavucontrol \
   grim slurp wl-clipboard brightnessctl dolphin kanshi wdisplays \
   xdg-desktop-portal-wlr polkit-kde-agent-1 okular \
   libwebp7 librsvg2-common gsettings-desktop-schemas \
   wpasupplicant firmware-linux firmware-linux-nonfree \
   curl gir1.2-nm-1.0 gir1.2-nma-1.0 qt5ct qt6ct kde-style-breeze \
-  cups cups-client cups-bsd gtklp firefox-esr chromium
+  cups cups-client cups-bsd gtklp firefox-esr qutebrowser
 
 
 sudo usermod -aG netdev $USER
